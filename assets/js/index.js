e19d3ab526dd6e67b2d04d2537c5f6c53ec4ebac
Daniel Acosta <dan_draco@hotmail.com>
2026-03-20 00:30:43 -0600

    refactorizacion de elementos, index raiz

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+// Get query parameters
+const params = new URLSearchParams(window.location.search);
+const id = parseInt(params.get('id'));           // convert to number
+const language = params.get('language') || 'spanish'; // default to english
+
+// Fetch JSON file
+fetch('inventory.json')
+  .then(response => response.json())
+  .then(data => {
+    const item = data.find(obj => obj.id == id);
+    if (item) {
+        const content = item[language.toLowerCase()] || 'Language not found';
+        document.getElementById('content').textContent = content;
+        const title = 'title_' + language.toLowerCase();
+        document.getElementById('title').textContent = item[title];
+        const audio = 'audio_' + language.toLowerCase();
+    
+        //set the audio
+        const audioPath = item[audio];
+        const audioElement = document.getElementById('audio');
+        
+        if (audioPath) {
+          audioElement.src = audioPath;
+          audioElement.load();  // load the new audio file
+          const img = document.getElementById('image');
+          img.src = item['image']
+        } else {
+          audioElement.style.display = 'none'; // hide audio if no file
+        }
+
+    } else {
+      document.getElementById('content').textContent = 'Item not found';
+    }
+    document.getElementById('author').textContent = item['author'];
+  })
+  .catch(err => {
+    console.error(err);
+    document.getElementById('content').textContent = 'Error loading data';
+  });
